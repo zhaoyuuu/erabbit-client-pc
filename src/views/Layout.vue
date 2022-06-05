ea09e9f1e918a59b45f5f046f26f36f7c8419d48
yuhang <zyh200286@qq.com>
2022-06-05 14:49:14 +0800

    init

---
+++ src/views/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='container'>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  setup () {
+
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+
+</style>
